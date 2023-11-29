59cc37b33a7203133bfcf4c664c6966b56233b7b
Pablo Rigueto <pabloedurigueto@outlook.com>
2023-11-29 16:20:51 -0300

    get almost all data.

--- themes/custom/dc/js/src/mainPage/index.jsx
+++ themes/custom/dc/js/src/mainPage/index.jsx
@@ -22,13 +22,30 @@ const SpotifyDataComponent = () => {
       {isError && <p style={{ color: 'red' }}>Error fetching data. Please try again later.</p>}
       {data && (
         <div>
+          {console.log(data.items)}
           <h2>Top 20 Tracks</h2>
           {data.items.map((track, index) => (
             <div key={index}>
               <p>{track.track.name}</p>
               {/* Add more details about the track as needed */}
               <img src={track.track.album.images[0].url} alt={track.track.name} />
-              <a href={`/track/${track.track.id}`}>Details</a>
+              <div> Track ID: { track.track.id } </div>
+
+              <div> Added At: { track.added_at } </div>
+
+              <div> Album Type: { track.track.album.album_type } </div>
+
+              <div> Artists ID: { track.track.artists[0].id } </div>
+              <div> Artists Name: { track.track.artists[0].name } </div>
+
+              <div> Music ID: { track.track.id } </div>
+              <div> Music Name: { track.track.name } </div>
+              <div> Music Popularity: { track.track.popularity } </div>
+              <div> Music Number: { track.track.track_number } </div>
+
+              <div> Available Markets { track.track.available_markets } </div>
+
+
             </div>
           ))}
         </div>
