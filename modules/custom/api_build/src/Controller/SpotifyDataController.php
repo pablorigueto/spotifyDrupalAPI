2ef3afbf967ae3a67308e55a51a2b72dd1610463
Pablo Rigueto <pabloedurigueto@outlook.com>
2023-11-29 22:20:36 -0300

    Saving correctly the nodes.

--- modules/custom/api_build/src/Controller/SpotifyDataController.php
+++ modules/custom/api_build/src/Controller/SpotifyDataController.php
@@ -3,14 +3,42 @@
 namespace Drupal\api_build\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Controller routines for Spotify data.
  */
 class SpotifyDataController extends ControllerBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new SpotifyDataController object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Handles the submission of Spotify data.
@@ -23,27 +51,38 @@ class SpotifyDataController extends ControllerBase {
    */
   public function submitSpotifyData(Request $request) {
     try {
+      // Count existing nodes of type 'spotify'.
+      $query = $this->entityTypeManager->getStorage('node')->getQuery()
+        ->condition('type', 'spotify')
+        ->accessCheck(FALSE);
+      $count = $query->count()->execute();
+
+      // Check if the count is less than 20.
+      if ($count >= 20) {
+        return new JsonResponse(['error' => 'Cannot save more than 20 Spotify nodes.'], 400);
+      }
 
       $data = json_decode($request->getContent(), true);
 
-      // Create a new node of type 'spotify'.
-      $node = Node::create([
-        'type' => 'spotify',
-        'title' => $data['track_name'], // You can set the title to the track name or any other relevant information.
-        'field_track_added_at' => $data['track_added_at'],
-        'field_track_album_image_url' => $data['track_album_image_url'],
-        'field_track_album_type' => $data['track_album_type'],
-        'field_track_artist_id' => $data['track_artist_id'],
-        'field_track_artist_name' => $data['track_artist_name'],
-        'field_track_id' => $data['track_id'],
-        'field_track_main_genre' => $data['track_main_genre'],
-        'field_track_name' => $data['track_name'],
-        'field_track_number' => $data['track_number'],
-        'field_track_popularity' => $data['track_popularity'],
-      ]);
+      foreach ($data['data'] as $trackData) {
+        // Create a new node of type 'spotify'.
+        $node = Node::create([
+          'type' => 'spotify',
+          'title' => $trackData['track_name'],
+          'field_track_added_at' => $trackData['track_added_at'],
+          'field_track_album_image_url' => $trackData['track_album_image_url'],
+          'field_track_album_type' => $trackData['album_type'],
+          'field_track_artist_id' => $trackData['artist_id'],
+          'field_track_artist_name' => $trackData['artist_name'],
+          'field_track_id' => $trackData['track_id'],
+          'field_track_main_genre' => $trackData['track_main_genre'],
+          'field_track_name' => $trackData['track_name'],
+          'field_track_number' => $trackData['track_number'],
+          'field_track_popularity' => $trackData['popularity'],
+        ]);
 
-      // Save the node.
-      $node->save();
+        $node->save();
+      }
 
       return new JsonResponse(['message' => 'Spotify data saved successfully.']);
     } catch (\Exception $e) {
