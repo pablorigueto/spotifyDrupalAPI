02c20fe64b16cb0ca9606e803a7b94403d3ba166
Pablo Rigueto <pabloedurigueto@outlook.com>
2023-11-30 22:05:04 -0300

    base

--- themes/custom/dc/js/dist_dev/custom.js
+++ themes/custom/dc/js/dist_dev/custom.js
@@ -1,6 +1,6 @@
 (function ($, once) {
   Drupal.behaviors.addHomeMenuLinks = {
-    attach(context) {
+    attach() {
       // Run this code once
       if (once.addHomeMenuLinks) {
         return;
@@ -15,4 +15,58 @@
       $('.path-home #block-dc-breadcrumbs ol').append(newLink);
     },
   };
+
+  Drupal.behaviors.addArtistsMenuLinks = {
+    attach() {
+      // Run this code once
+      if (once.addArtistsMenuLinks) {
+        return;
+      }
+      once.addArtistsMenuLinks = true;
+
+      // Replace Home with /home.
+      $('.path-artists #block-dc-breadcrumbs a[href="/"]').attr('href', '/home').text('Home');
+
+      // Add a new breadcrumb item /songs.
+      const newLink = $('<li><a href="/songs">Songs</a></li>');
+      $('.path-artists #block-dc-breadcrumbs ol').append(newLink);
+    },
+  };
+
+  Drupal.behaviors.addSongsMenuLinks = {
+    attach() {
+      // Run this code once
+      if (once.addSongsMenuLinks) {
+        return;
+      }
+      once.addSongsMenuLinks = true;
+
+      // Replace Home with /artists.
+      $('.path-songs #block-dc-breadcrumbs a[href="/"]').attr('href', '/home').text('Home');
+
+      // Add a new breadcrumb item /artists.
+      const newLink = $('<li><a href="/artists">Artists</a></li>');
+      $('.path-songs #block-dc-breadcrumbs ol').append(newLink);
+    },
+  };
+
+  Drupal.behaviors.addBackButton = {
+    attach(context, settings) {
+      // Run this code once
+      if (once.addBackButton) {
+        return;
+      }
+      once.addBackButton = true;
+
+      // Create a new div element
+      var backButtonDiv = '<div class="node__back"><a href="/home">Back</a></div>';
+
+      // Get the parent element where you want to prepend the new div
+      var parentElement = $('.page-node-type-spotify #block-dc-content', context);
+
+      // Prepend the new div to the parent element
+      parentElement.prepend(backButtonDiv);
+    },
+  };
+
 })(jQuery, once);
